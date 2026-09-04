803239ee7f9a3ae3309a7e16915039554de4b544
one-media-asia <peterjgreaney@icloud.com>
2026-09-04 04:19:22 +0000

    Remove unused Next.js config (site is static HTML)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-    githubToken: process.env.GITHUB_TOKEN,
-  },
-};
-
-module.exports = nextConfig;
